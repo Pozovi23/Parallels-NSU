a875003d075318333a104d2eeb80444d0f43d2b4
Gleb_Zhigalov <g.zhigalov@g.hsu.ru>
2025-02-03 17:44:36 +0000

    task_1_make

---
+++ task_1/task_1_make.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+#define TYPENAME float
+// #define TYPENAME double
+#define AMOUNT_OF_ELEMENTS 10000000
+
+int main() {
+  TYPENAME *sinusMas = new TYPENAME[AMOUNT_OF_ELEMENTS];
+  TYPENAME summ = 0;
+
+  for (int i = 0; i < AMOUNT_OF_ELEMENTS; i++) {
+    sinusMas[i] = std::sin((2 * M_PI * i) / AMOUNT_OF_ELEMENTS);
+    summ += sinusMas[i];
+  }
+
+  std::cout << summ << std::endl;
+  delete[] sinusMas;
+  return 0;
+}
